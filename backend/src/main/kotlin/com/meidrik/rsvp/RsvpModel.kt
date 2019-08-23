a3ee6c14c1f0aba723a46e57caedf1063b70f9a3
Fredrik Bystam <fredrik.bystam@kry.se>
2019-08-23 11:36:49 +0200

    fix

--- backend/src/main/kotlin/com/meidrik/rsvp/RsvpModel.kt
+++ backend/src/main/kotlin/com/meidrik/rsvp/RsvpModel.kt
@@ -7,7 +7,7 @@ import javax.persistence.*
 @Entity
 data class Rsvp(
     @Id val id: UUID = UUID.randomUUID(),
-    @Column(nullable = false) val name: String,
     @Column(nullable = false) val time: Instant = Instant.now(),
+    @Column(nullable = false) val name: String,
     @Column(nullable = true) val foodPreferences: String?
 )
